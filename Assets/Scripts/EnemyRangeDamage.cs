5311cdf4a7c0ed3c1ad041c5aa305181fc808e26
Thomas Tran <Thomas Tran>
2017-01-28 21:59:39 -0800

    Fixed Vampire animations

---
+++ Assets/Scripts/EnemyRangeDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRangeDamage : MonoBehaviour {
+    public int rangeDamage;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2d(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Health>().takeDamage(rangeDamage);
+        }
+        Debug.Log("HELLO");
+    }
+}
