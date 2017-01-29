84e6ce0d5b6a454a2d530ac1a091ccc05669c769
dbelliss <daveybelliss@yahoo.com>
2017-01-28 22:36:47 -0800

    Adding music

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour {
     public int damageDealt;
-
+	AudioSource aud;
 	// Use this for initialization
 	void Start () {
-
+		aud = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -19,6 +19,8 @@ public class PlayerDamage : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("Enemy"))
         {
+			
+			aud.Play();
             other.GetComponent<Health>().takeDamage(damageDealt);
         }
     }
