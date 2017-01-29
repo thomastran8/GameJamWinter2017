cb64d8f2a9b41f947c31fe048620b2eb75caa2da
dbelliss <daveybelliss@yahoo.com>
2017-01-28 18:44:57 -0800

    menu screen

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void startGame(){
+		SceneManager.LoadScene (2);
+	}
+
+	public void quit() {
+		Application.Quit();
+	}
+
+	public void help() {
+		SceneManager.LoadScene (1);
+	}
+}
+
