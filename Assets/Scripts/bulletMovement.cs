6515b7f9354d17d05aed7b0188179b028a075443
dbelliss <daveybelliss@yahoo.com>
2017-02-10 17:41:41 -0800

    updating wolf sprite. Starting Rangewold.

---
+++ Assets/Scripts/bulletMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletMovement : MonoBehaviour {
+
+	public float speed = 1;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		transform.position = transform.position + speed * new Vector3 (1, 0, 0);
+	}
+}
